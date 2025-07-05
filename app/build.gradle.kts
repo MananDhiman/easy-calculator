3a26c40b0bde07236f7ebc60c81fb97168bb19fe
Manan Dhiman <manan.dhiman12@gmail.com>
2025-07-05 20:31:31 +0530

    Version code changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
     applicationId = "manandhiman.easycalculator"
     minSdk = 22
     targetSdk = 35
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 4
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
